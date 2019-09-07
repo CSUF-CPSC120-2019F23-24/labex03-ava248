c1dedbe82f5525632329f55215cf8159c8a3ea5e
avahess <avahess@cs104u-3f.acad.fullerton.edu>
2019-09-07 14:08:31 -0700

    wrote a program to calculate height into feet & inches

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,26 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+
+int main()
+{
+
+  int height;
+  int feet;
+  int inches;
+
+
+// Get the person's height
+  std::cout << "Please enter the person's height in inches: ";
+  std::cin >> height;
+
+  feet = height / 12;
+  inches = height % 12;
+
+  std::cout << "That person is " << feet << '\'' << inches << '"' << std::endl;
+
+
+
+
+  return 0;
+}
